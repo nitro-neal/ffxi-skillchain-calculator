574856f1621ca590bfd1758a5bbbc9d01e0c67a0
ARoess16 <124461356+ARoess16@users.noreply.github.com>
2023-04-01 19:59:27 -0500

    Adding Images to JobTiles and other functionality (#5)

--- src/components/CharacterTiles.js
+++ src/components/CharacterTiles.js
@@ -1,60 +1,122 @@
-import React, { Fragment, ReactDOM, ReactMotion, useState, useEffect } from "react";
+import React, {
+  Fragment,
+  ReactDOM,
+  ReactMotion,
+  useState,
+  useEffect,
+} from "react";
 import "../App.css";
+
+import war from "./img/war.webp";
+import bst from "./img/bst.webp";
+import drg from "./img/drg.webp";
+import drk from "./img/drk.webp";
+import mnk from "./img/mnk.webp";
+import nin from "./img/nin.webp";
+import pld from "./img/pld.webp";
+import rdm from "./img/rdm.webp";
+import rng from "./img/rng.webp";
+import sam from "./img/sam.webp";
+import smn from "./img/smn.webp";
+import thf from "./img/thf.webp";
+import whm from "./img/whm.webp";
+import blm from "./img/blm.webp";
+import brd from "./img/brd.webp";
+
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBRipple,
+  MDBCardHeader,
+  MDBCardFooter,
+} from "mdb-react-ui-kit";
+import { Slide, imageListClasses } from "@mui/material";
 
 function CharacterTiles() {
   const [jobtiles, setjobtiles] = useState([
-    { Name: "Warrior", id: "war", value: "war" },
-    { Name: "Thief", id: "thf", value: "thf" },
-    { Name: "Dark Knight", id: "drk", value: "drk" },
-    { Name: "Paladin", id: "pld", value: "pld" },
-    { Name: "Beast Master", id: "bst", value: "bst" },
-    { Name: "Dragoon", id: "drg", value: "drg" },
-    { Name: "Monk", id: "mnk", value: "mnk" },
-    { Name: "Ninja", id: "nin", value: "nin" },
-    { Name: "Red Mage", id: "rdm", value: "rdm" },
-    { Name: "Ranger", id: "rng", value: "rng" },
-    { Name: "Samurai", id: "sam", value: "sam" },
-    { Name: "Summoner", id: "smn", value: "smn" },
+    { Name: "Warrior", id: "war", value: "war", img: war },
+    { Name: "Monk", id: "mnk", value: "mnk", img: mnk },
+    { Name: "Red Mage", id: "rdm", value: "rdm", img: rdm },
+    { Name: "Thief", id: "thf", value: "thf", img: thf },
+    { Name: "Dark Knight", id: "drk", value: "drk", img: drk },
+    { Name: "Paladin", id: "pld", value: "pld", img: pld },
+    { Name: "Beast Master", id: "bst", value: "bst", img: bst },
+    { Name: "Dragoon", id: "drg", value: "drg", img: drg },
+    { Name: "Ninja", id: "nin", value: "nin", img: nin },
+    { Name: "Ranger", id: "rng", value: "rng", img: rng },
+    { Name: "Samurai", id: "sam", value: "sam", img: sam },
+    { Name: "Summoner", id: "smn", value: "smn", img: smn },
   ]);
 
-  useEffect(() => {
-    //console.log("useEffect Fired");
-    createTiles();
-    return () => {};
-  }, []);
+  const topROW = [];
+  const bottomROW = [];
 
-  function createTiles() {
-    for (let i = 0; i < jobtiles.length / 2; i++) {
-      const list = document.getElementById("begin1");
-      const temp = document.createElement("div");
-      temp.title = jobtiles[i].Name;
-      temp.className = "jobtile col";
-      temp.id = jobtiles[i].id;
-      temp.value = jobtiles[i].value;
-      temp.innerHTML = jobtiles[i].Name;
-      temp.draggable = true;
-      list.appendChild(temp);
-    }
+  for (let i = 0; i < jobtiles.length / 2; i++) {
+    topROW.push(
+      <div
+        draggable="true"
+        className="jobtile col"
+        value={jobtiles[i].value}
+        id={jobtiles[i].id}
+        title={jobtiles[i].Name}
+      >
+        <MDBCard>
+          <MDBCardImage
+            className="img-fluid"
+            src={jobtiles[i].img}
+            position="top"
+            alt="..."
+            style={{ maxWidth: "15em", maxHeight: "15em" }}
+          />
+          <MDBCardBody>
+            <MDBCardTitle>{jobtiles[i].Name}</MDBCardTitle>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    );
+  }
 
-    for (let i = jobtiles.length / 2; i < jobtiles.length; i++) {
-      const list = document.getElementById("begin2");
-      const temp = document.createElement("div");
-      temp.title = jobtiles[i].Name;
-      temp.className = "jobtile col";
-      temp.id = jobtiles[i].id;
-      temp.value = jobtiles[i].value;
-      temp.innerHTML = jobtiles[i].Name;
-      temp.draggable = true;
-      list.appendChild(temp);
-    }
+  for (let i = jobtiles.length / 2; i < jobtiles.length; i++) {
+    bottomROW.push(
+      <div
+        draggable="true"
+        className="jobtile col"
+        value={jobtiles[i].value}
+        id={jobtiles[i].id}
+        title={jobtiles[i].Name}
+      >
+        <MDBCard>
+          <MDBCardImage
+            className="img-fluid"
+            src={jobtiles[i].img}
+            position="top"
+            alt="..."
+            style={{ maxWidth: "15em", maxHeight: "15em" }}
+          />
+          <MDBCardBody>
+            <MDBCardTitle>{jobtiles[i].Name}</MDBCardTitle>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    );
   }
 
   return (
     <Fragment>
-      <div>
-        <div className="begin1 row" id="begin1"></div>
-        <div className="begin2 row" id="begin2"></div>
-      </div>
+      <Slide timeout={400} direction="right" in={true}>
+        <div className="row">{topROW}</div>
+      </Slide>
+
+      <Slide timeout={400} direction="left" in={true}>
+        <div className="row">{bottomROW}</div>
+      </Slide>
     </Fragment>
   );
 }
--- src/components/JobWeaponSelect.js
+++ src/components/JobWeaponSelect.js
@@ -1,4 +1,9 @@
-import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdb-react-ui-kit";
+import {
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from "mdb-react-ui-kit";
 
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
@@ -9,11 +14,7 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 export default function JobWeaponSelect(props) {
   if (Object.keys(props.selectedJob).length == 0) {
-    return (
-      <>
-        <p>select party memmber</p>
-      </>
-    );
+    return <></>;
   }
 
   let weapons = [];
@@ -28,10 +29,20 @@ export default function JobWeaponSelect(props) {
         <FormControl fullWidth>
           {/* TODO Why is default value not working? */}
           <InputLabel id="demo-simple-select-label">Weapon</InputLabel>
-          <Select defaultValue={weapons[0]} onChange={(e) => props.moveChanged(e)}>
+          <Select
+            // defaultValue={weapons[0]}
+            // value={weapons[0]}
+            onChange={(e) => {
+              props.moveChanged(e);
+              // console.log("HOWDY:", weapons[0]);
+            }}
+          >
             {weapons.map((w) => {
               return (
-                <MenuItem name={props.selectedJob.name} value={props.selectedJob.name + "_" + w}>
+                <MenuItem
+                  name={props.selectedJob.name}
+                  value={props.selectedJob.name + "_" + w}
+                >
                   {w}
                 </MenuItem>
               );
